e65af8ae7df68d61c1daf130aaca826ea68ff753
lightshire <giansantillan18@gmail.com>
2014-02-05 08:32:46 +0800

    fixed navbar errors

--- app/views/index/nav.blade.php
+++ app/views/index/nav.blade.php
@@ -6,7 +6,7 @@
 			<span class="icon-bar"></span>
 			<span class="icon-bar"></span>
 		</button>
-		<a class="navbar-brand" href="#">Philippine Disaster Information Service</a>
+		<a class="navbar-brand" href="/">Philippine Disaster Information Service</a>
 	</div>
 	<div class="collapse navbar-collapse" id="main-nav">
 		<ul class="navbar-nav nav navbar-right">
